22ef87c9cb3462a7f9ee9aff8fffa2813cb00808
Matheus Pacheco <matheusavpacheco@gmail.com>
2017-10-23 22:44:04 -0600

    Working grocery list generation

--- choosemeals.js
+++ choosemeals.js
@@ -8,7 +8,7 @@ function mainCtrl ($scope)
     $scope.budget = 100;
     $scope.remaining=$scope.budget;
     console.log("mainCtrl");
-    
+
     $scope.addItem = function (item)
     {
         console.log("I made it to the function");
@@ -19,21 +19,21 @@ function mainCtrl ($scope)
             $scope.remaining = $scope.budget - $scope.total;
             console.log($scope.remaining);
         }
-        
+
         if (item == "Pasta")
         {
             $scope.cart.push({Item: "Shrimp Scampi with Pasta", price: 24.29, done:false});
             $scope.total += 24.29;
-            
+
             $scope.remaining = $scope.budget - $scope.total;
             console.log($scope.remaining);
         }
-        
+
         if (item == "Chili")
         {
             $scope.cart.push({Item: "Creamy White Chili", price: 11.28, done:false});
             $scope.total += 11.28;
-            
+
             $scope.remaining = $scope.budget - $scope.total;
             console.log($scope.remaining);
         }
@@ -42,11 +42,11 @@ function mainCtrl ($scope)
         {
             $scope.cart.push({Item: "Orange, Honey and Soy Chicken", price: 15.56, done:false});
             $scope.total += 15.56;
-            
+
             $scope.remaining = $scope.budget - $scope.total;
             console.log($scope.remaining);
         }
-        
+
         for(var i  = 0; i <$scope.cart.length; i++)
         {
             console.log($scope.cart[i].Item);
@@ -70,12 +70,95 @@ function mainCtrl ($scope)
     };
     $scope.generateGroceryList = function()
     {
-        /*
-        Now the customer gets sent to the next webpage...
-        */
-        
-        
-        
+        var meals = $scope.cart;
+        console.log(meals);
+        $scope.totalCost = 0;
+
+        //Spaghetti
+        $scope.groundBeef = 0;
+        $scope.spaghetti = 0;
+        $scope.pastaSauce = 0;
+
+        //Hamburgers
+        $scope.buns = 0;
+        $scope.tomatoes = 0;
+        $scope.onions = 0;
+        $scope.lettuce = 0;
+        //groundbeef
+
+        $scope.finalList = [];
+
+
+        angular.forEach(meals,function(x) {
+
+            console.log("\nIteration " + x.Item);
+
+            if(x.Item == "Mexican Lasagna II"){
+
+                $scope.finalList.push({Item:"Spaghetti"});
+                $scope.groundBeef += 1;
+                $scope.totalCost += 3.99;
+
+                $scope.spaghetti += 1;
+                $scope.totalCost += 1.50;
+
+                $scope.pastaSauce += 1;
+                $scope.totalCost += 2.50;
+
+            }
+            else if(x.Item == "Shrimp Scampi with Pasta"){
+
+               // console.log("\nBurgers" + i);
+
+                $scope.finalList.push({Item:"Burgers"});
+
+                $scope.groundBeef += 1;
+                $scope.totalCost += 3.99;
+
+                $scope.buns += 1;
+                $scope.totalCost += 2;
+
+                $scope.tomatoes += 1;
+                $scope.totalCost += .50;
+
+            }
+            else if(x.Item == "Creamy White Chili"){
+
+                //console.log("\nBurgers" + i);
+
+                $scope.finalList.push({Item:"Burgers"});
+
+                $scope.groundBeef += 1;
+                $scope.totalCost += 3.99;
+
+                $scope.buns += 1;
+                $scope.totalCost += 2;
+
+                $scope.tomatoes += 1;
+                $scope.totalCost += .50;
+
+            }
+            else if(x.Item == "Orange, Honey and Soy Chicken"){
+
+                //console.log("\nBurgers" + i);
+
+                $scope.finalList.push({Item:"Burgers"});
+
+                $scope.groundBeef += 1;
+                $scope.totalCost += 3.99;
+
+                $scope.buns += 1;
+                $scope.totalCost += 2;
+
+                $scope.tomatoes += 1;
+                $scope.totalCost += .50;
+
+            }
+            
+            
+        });   
+        console.log($scope.finalList);
+
     }
 
 }
